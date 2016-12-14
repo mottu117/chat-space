7b1f2f5451f011a78ed1a33ed385c0f10df36827
mottu117 <motomotok55@gmail.com>
2016-12-14 20:54:27 +0900

    [WIP]途中経過セーブ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,17 @@
 class GroupsController < ApplicationController
-    def edit; end
+    def edit
 
-    def new; end
+    end
+
+
+    def new
+
+    end
+
+
+    def create
+      Group.create(group_name: params[:chat_group_name])
+      redirect_to root_path, notice: 'グループを作成しました。'
+
+    end
 end
