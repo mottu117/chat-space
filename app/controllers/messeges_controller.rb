d081a20fa6458b46a2d84ba412a5d6911cea038d
mottu117 <motomotok55@gmail.com>
2016-12-24 19:03:05 +0900

    ご指摘対応①
    インデックスをメッセージコントローラに持たせる。
    グループコントローラから移管
    
    メッセージリソースをグループリソース配下にネスト

--- app/controllers/messeges_controller.rb
+++ app/controllers/messeges_controller.rb
@@ -2,6 +2,10 @@ class MessegesController < ApplicationController
     before_action :authenticate_user!
 
     def index
+        @find_id = "86"
+        @group_sample_name = Group.find(@find_id)
+        @group_sample_member = GroupsUser.where(groups_users: {group_id: @find_id} )
+        @user_joined_group_lists = Group.includes(:groups_users).where(groups_users: {user_id: current_user.id} )
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'groups#index'
+  root 'messeges#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :messeges, only: [:show, :new]
-  resources :groups, only: [:index, :edit, :update, :new, :create]
+
+  resources :groups, only: [:edit, :update, :new, :create] do
+    resources :messeges, only: [:index, :new]
+  end
 
 end
