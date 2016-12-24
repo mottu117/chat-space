6b1052eccfd478355ad9c0cd4fbabaf414cafbfe
mottu117 <motomotok55@gmail.com>
2016-12-24 16:14:53 +0900

    Groupのshowアクションは不要のようなので削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :messeges, only: [:show, :new]
-  resources :groups, only: [:index, :show,:edit, :new, :create]
+  resources :groups, only: [:index,:edit, :new, :create]
 end
