50a5fdd8d4961e50f3087c5e64777e33aa8df6e0
mottu117 <motomotok55@gmail.com>
2016-12-25 14:14:46 +0900

    ご指摘対応
    アソシエーションが使いこなせていなかった部分を修正。
    それにあたり、group_idが取れていないとfindがエラーするので、if分岐復活。

--- app/controllers/messeges_controller.rb
+++ app/controllers/messeges_controller.rb
@@ -4,8 +4,9 @@ class MessegesController < ApplicationController
     def index
         @user_joined_group_lists = Group.includes(:groups_users).where(groups_users: {user_id: current_user.id} )
         @find_id = params[:group_id]
-        @group_name = Group.find(@find_id)
-        @group_member = GroupsUser.where(groups_users: {group_id: @find_id} )
+        if @find_id
+          @group_info = Group.find(@find_id)
+        end
     end
 
 end
