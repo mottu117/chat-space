1b4a6320cc24088621325b3cab375d6da941fd26
mottu117 <motomotok55@gmail.com>
2017-01-05 19:02:31 +0900

    ご指摘対応
    ・Viewでロジック記述していたところは、ヘルパーメソッドに置き換え
    ・部分テンプレートの増加を見越し、ディレクトリ・ファイル再配置
    ・Viewでeachしているところは、部分テンプレート使用に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  # application_helper.rbに集約したメソッドには、apphelperを冠するものとする。
+  # どこに記載されている処理なのかわかりにくくなるので。
+
+  def apphelper_show_flash_massage(flash_alert,flash_notice)
+    if flash_alert != nil
+      content_tag("div", flash.now[:alert], class: "flash_message--aleat")
+    elsif flash_notice != nil
+      content_tag("div", flash.now[:notice], class: "flash_message--notice")
+    end
+  end
+
+  def apphelper_show_text_boolean(true_text,false_text)
+    if true_text
+      true_text
+    else
+      false_text
+    end
+  end
 end
