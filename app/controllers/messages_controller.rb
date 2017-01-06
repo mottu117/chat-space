275c97d46bfa05eda0f8fd2ff7982a9dfda9c12a
mottu117 <motomotok55@gmail.com>
2017-01-06 16:29:44 +0900

    メッセージ機能導入
    　　・メッセージ投稿
    　　・メッセージテキスト必須化（HTMLでのリクワイアド、バリデーション）
    　　・投稿成否のフラッシュメッセージ

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,23 @@ class MessagesController < ApplicationController
     def index
         @current_user_groups = appfunc_get_current_user_groups # application_controllerより継承
         @group = Group.find(params[:group_id])
+
+        @message = Message.new # form_forに渡す
+        @index_messages = Message.where(group_id: params[:group_id]) # messageの一覧表示データ
+    end
+
+    def create
+        @input_message = Message.new(message_params)
+        if @input_message.save
+            redirect_to group_messages_path, notice: 'メッセージを投稿しました。'
+        else
+            redirect_to group_messages_path, alert: 'メッセージを投稿できませんでした。'
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:text, :image_url).merge(group_id: params[:group_id], user_id: current_user.id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :groups, only: [:index, :edit, :update, :new, :create] do
-        resources :messages, only: [:index, :new]
+        resources :messages, only: [:index, :create]
     end
 end
