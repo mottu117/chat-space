69c406a7b7a976da014c838a80b6ed47c5860f1e
mottu117 <motomotok55@gmail.com>
2017-01-06 12:04:12 +0900

    コントローラ名とその関連修正（Messageの綴り誤り）
    messege → message
    やっちまったな。。。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,4 @@
-class MessegesController < ApplicationController
+class MessagesController < ApplicationController
     def index
         @current_user_groups = appfunc_get_current_user_groups # application_controllerより継承
         @group = Group.find(params[:group_id])
