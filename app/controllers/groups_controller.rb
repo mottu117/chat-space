fc13aa70a6075709f09ea7a87c16f7d4d645fdba
mottu117 <motomotok55@gmail.com>
2017-01-07 11:59:50 +0900

    ご指摘対応
    ・グループ作成・編集時エラー遷移処理を、リダイレクトからレンダーに変更
    ・ついでに、メッセージ通知をフラッシュからヘルパーメソッドに変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+    before_action :initialize_group_instance, only: [:new, :create]
     before_action :get_target_group, only: [:edit, :update]
 
     def index
@@ -6,15 +7,17 @@ class GroupsController < ApplicationController
     end
 
     def new
-        @group = Group.new
+        # before_actionによりGroupインスタンスを事前生成してある。
     end
 
     def create
+        # before_actionによりGroupインスタンスを事前生成してある。
         group = Group.new(group_params)
         if group.save
             redirect_to root_path, notice: 'グループを作成しました。'
         else
-            redirect_to root_path, alert: 'グループを作成できませんでした。グループ名が未入力の可能性があります。'
+            @error_message = 'グループを作成できませんでした。グループ名が未入力の可能性があります。'
+            render new_group_path
         end
     end
 
@@ -27,11 +30,16 @@ class GroupsController < ApplicationController
         if @target_group.update(group_params)
             redirect_to root_path, notice: 'グループを編集しました。'
         else
-            redirect_to root_path, alert: 'グループを編集できませんでした。グループ名が未入力の可能性があります。'
+            @error_message = 'グループを編集できませんでした。グループ名が未入力の可能性があります。'
+            render action: :edit
         end
     end
 
     private
+
+    def initialize_group_instance
+        @group = Group.new
+    end
 
     def group_params
         params.require(:group).permit(:group_name, user_ids: [])
