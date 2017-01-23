7fe180d5c3e1d3995c0b55b160f563062b7101a5
mottu117 <motomotok55@gmail.com>
2017-01-23 13:00:18 +0900

    日付の形式を整えるメソッド追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,10 @@ class Message < ApplicationRecord
   belongs_to :group
 
   validates :text, presence: true
+
+  def created_at_Prepared
+      output = created_at.strftime('%Y/%m/%d/ %H:%M:%S')
+      return output
+  end
+
 end
