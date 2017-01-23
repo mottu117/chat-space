8bc69174e04a9b316c57ebb5628448dce7c2ef89
mottu117 <motomotok55@gmail.com>
2017-01-23 12:02:11 +0900

    共通機能追加
    ・日付形式加工
    ・フラッシュコントロール
    ・自動スクロール

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,8 @@
 //= require turbolinks
 //= require_tree .
 
+
+
 //共通関数には、app_を冠するものとする。
 
 //文字列を指定日付形式へ加工する。ゼロパディングあり。
@@ -25,4 +27,33 @@ function app_toLocaleString(date) {
             ('0' + date.getDate()).slice(-2)
         ].join('/') + ' ' +
         date.toLocaleTimeString();
+}
+
+//フラッシュメッセージコントロール flash_sign = (alert: 警告, notice: 情報, reset: 表示メッセージ除去)
+function app_controll_Flash_Message(flash_sign, message) {
+    if (flash_sign === 'alert') {
+        $(document.body).prepend('<div class="flash_message--aleat">' + message);
+
+        $('.flash_message--aleat').fadeOut(10000).queue(function() {
+            this.remove();
+        });
+
+    } else if (flash_sign === 'notice') {
+        $(document.body).prepend('<div class="flash_message--notice">' + message);
+
+        $('.flash_message--notice').fadeOut(3000).queue(function() {
+            this.remove();
+        });
+
+    } else if (flash_sign === 'reset') {
+        $('.flash_message--aleat').remove();
+        $('.flash_message--notice').remove();
+    }
+}
+
+//最下まで自動スクロール(target_selector = アニメーションさせるセレクタ, index_selector = アニメーション量/高さ を取得するセレクタ )
+function app_goBottom(target_selector, index_selector) {
+  $(target_selector).animate({
+      scrollTop: $(index_selector)[0].offsetHeight
+  }, 'normal');
 }
