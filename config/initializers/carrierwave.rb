ed21bf12d92beee31a0d4f732504369e334a722e
mottu117 <motomotok55@gmail.com>
2017-02-03 16:34:10 +0900

    S3対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,14 @@
 CarrierWave.configure do |config|
-  config.asset_host = 'http://localhost:3000'
+  config.fog_credentials = {
+    provider: Settings.AWS_S3_PROVIDER,
+    aws_access_key_id: Settings.AWS_S3_API_KEY,
+    aws_secret_access_key: Settings.AWS_S3_API_SECRET,
+    region: Settings.AWS_S3_REGION,
+  }
+
+  config.fog_directory = Settings.AWS_S3_FOG_DIRECTRY
+  config.asset_host = Settings.AWS_S3_ASSET_HOST
+
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ #ファイル名文字化け対応
+
 end
